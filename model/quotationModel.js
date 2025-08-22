bb37e972e3ec50f5d67b81ef0eacea9834ee6e9d
TheerasakRung <rungworking@gmail.com>
2025-08-22 14:39:15 +0700

    customer_purchase

--- model/quotationModel.js
+++ model/quotationModel.js
@@ -96,7 +96,7 @@ const Customer = sequelize.define(
       type: DataTypes.STRING(13),
       allowNull: false,
     },
-    cus_purchase: {
+    customer_purchase: {
       type: DataTypes.STRING(40),
       allowNull: false,
     },
